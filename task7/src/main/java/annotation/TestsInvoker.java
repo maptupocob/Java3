9f6a89c2e865e1937734ec391e384f26255cb24f
maptupocob <30635657+maptupocob@users.noreply.github.com>
2019-11-29 13:45:33 +0300

    refactor. Add test for single Before and After.

--- task7/src/main/java/annotation/TestsInvoker.java
+++ task7/src/main/java/annotation/TestsInvoker.java
@@ -2,8 +2,10 @@ package annotation;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 public class TestsInvoker {
 
@@ -11,34 +13,41 @@ public class TestsInvoker {
         Tests tests = new Tests();
         Class cl = Tests.class;
         Method[] methods = cl.getDeclaredMethods();
-        Arrays.sort(methods, new Comparator<Method>() {
-            @Override
-            public int compare(Method o1, Method o2) {
-                if (o1.getDeclaredAnnotation(BeforeSuit.class) != null) {
-                    return -1;
-                } else if (o2.getDeclaredAnnotation(BeforeSuit.class) != null) {
-                    return 1;
-                } else if (o1.getDeclaredAnnotation(AfterSuit.class) != null) {
-                    return 1;
-                } else if (o2.getDeclaredAnnotation(AfterSuit.class) != null) {
-                    return -1;
-                } else {
-                    return (o1.getDeclaredAnnotation(Test.class).priority() - o2.getDeclaredAnnotation(Test.class).priority());
-                }
-            }
-        });
 
+        List<Method> beforeMethods = new ArrayList<>();
+        List<Method> afterMethods = new ArrayList<>();
+        List<Method> testMethods = new ArrayList<>();
         for (Method m : methods) {
-            try {
-                m.setAccessible(true);
-                m.invoke(tests, null);
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            }
+            if (m.getDeclaredAnnotation(BeforeSuit.class) != null) beforeMethods.add(m);
+            if (m.getDeclaredAnnotation(AfterSuit.class) != null) afterMethods.add(m);
+            if (m.getDeclaredAnnotation(Test.class) != null) testMethods.add(m);
         }
 
+        Collections.sort(testMethods, new Comparator<Method>() {
+            @Override
+            public int compare(Method o1, Method o2) {
+                return (o1.getDeclaredAnnotation(Test.class).priority() - o2.getDeclaredAnnotation(Test.class).priority());
+            }
+        });
+        try {
+            if (beforeMethods.size() > 1) {
+                throw new RuntimeException("More than 1 method annotated BeforeSuit");
+            } else {
+                beforeMethods.get(0).invoke(tests, null);
+            }
+            for (Method testMethod : testMethods) {
+                testMethod.invoke(tests, null);
+            }
+            if (afterMethods.size() > 1) {
+                throw new RuntimeException("More than 1 method annotated AfterSuit");
+            } else {
+                afterMethods.get(0).invoke(tests, null);
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
